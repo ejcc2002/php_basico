01178f2633523509f2e5c742b7266a0a51944059
ejcc2002 <erickcarrion349@gmail.com>
2024-06-04 15:08:53 -0500

    Fin Practica PHP

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-$autenticando=true;
+/**$autenticando=true;
 $admin=false;
 
 if($autenticando && $admin){
     echo "Usuario autentico correctamente. ";}
 else{
     echo "Usuario no auntenticado, inicia sesión. ";
-} 
+} */ 
+ 
 
-<?php
-$edad= -12;
+/**$edad= -12;
 
 if($edad>0){
     if($edad >=18){
@@ -21,15 +21,69 @@ if($edad>0){
 }
 else {
     echo "Error, no existen edades negativas. ";
-}
+} */
 
-<?php
-$edad= 31;
+
+/**$edad= 31;
 if ($edad > 0 && $edad <=12) {
     echo "Usted, es un niño";
 } elseif ($edad>12 && $edad <= 35){
     echo "Usted, es un joven";
 } else {
     echo "Cuidate, estas envejeciendo. ";
-}
+} */
 
+
+/**$multiplicando=1;
+$multiplicador=2;
+$producto=0;
+
+for($multiplicando=1; $multiplicando<=10; $multiplicando++){
+    $producto=$multiplicando*$multiplicador;
+    echo "$multiplicando X $multiplicando: $producto. <br>";
+} */
+
+
+/**$multiplicando =1;
+$multiplicador =2;
+$producto =0;
+
+while($multiplicando <= 10){
+    $producto = $multiplicando * $multiplicador;
+    echo "$multiplicando X $multiplicador: $producto <br>";
+    $multiplicando++;
+} 
+
+
+$edades=[18,20,31,19,20];
+
+echo "Una de las edades que se encuentra en el arreglo es:". $edades[0];
+
+$edades=array(18,20,31,19,20);
+
+echo "Una de las edades que se encuentra en el arreglo es:". $edades[1]; */
+
+
+/**$cafes=array(
+    "Capuchino" => 50,
+    "Latte" => 49,
+    "Americano" => 70
+);
+
+echo "El precio del cafe Americano es de:{$cafes['Americano']}"; */
+ 
+$personas=array(
+    "Carlos" => array(
+        "Edad" => 20,
+        "Correo" => "carlos@espoch.edu.ec",
+        "Cedula" => 21004585933
+    ),
+    "Pedro" => array(
+        "Edad" => 31,
+        "Correo" => "pedro@espoch.edu.ec",
+        "Cedula" => 21004585934
+    )
+);
+
+echo "La edad de Pedro es: ". $personas["Pedro"]["Edad"];
+    
